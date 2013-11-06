fa2b1727b686bd2959fa29919a041d3db512d86d
niutech <jerzyglowacki@gmail.com>
2013-11-06 18:21:11 +0100

    Fixed infinite loop in Chrome

--- htmlpreview.js
+++ htmlpreview.js
@@ -14,47 +14,37 @@ var HTMLPreview = {
 
 	replaceAssets: function() {
 		var frame, a, link, script, i, href, src;
-		frame = [].concat.apply([].concat.apply([], document.getElementsByTagName('iframe')), document.getElementsByTagName('frame'));
+		frame = document.querySelectorAll('iframe[src],frame[src]');
 		for(i = 0; i < frame.length; ++i) {
-			if(frame[i].src) {
-				src = frame[i].src; //Get absolute URL
-				if(src.indexOf('//raw.github.com') > 0 || src.indexOf('//bitbucket.org') > 0) { //Check if it's from raw.github.com or bitbucket.org
-					frame[i].src = 'http://' + location.hostname + location.pathname + '?' + src; //Then rewrite URL so it can be loaded using YQL
-				}
+			src = frame[i].src; //Get absolute URL
+			if(src.indexOf('//raw.github.com') > 0 || src.indexOf('//bitbucket.org') > 0) { //Check if it's from raw.github.com or bitbucket.org
+				frame[i].src = 'http://' + location.hostname + location.pathname + '?' + src; //Then rewrite URL so it can be loaded using YQL
 			}
 		}
-		a = document.getElementsByTagName('a');
+		a = document.querySelectorAll('a[href]');
 		for(i = 0; i < a.length; ++i) {
-			if(a[i].href) {
-				href = a[i].href; //Get absolute URL
-				if(href.indexOf('#') > 0) { //Check if it's an anchor
-					a[i].href = 'http://' + location.hostname + location.pathname + location.search + '#' + a[i].hash.substring(1); //Then rewrite URL with support for empty anchor
-				}
-				else if(href.indexOf('//raw.github.com') > 0 || href.indexOf('//bitbucket.org') > 0) { //Check if it's from raw.github.com or bitbucket.org
-					a[i].href = 'http://' + location.hostname + location.pathname + '?' + href; //Then rewrite URL so it can be loaded using YQL
-				}
+			href = a[i].href; //Get absolute URL
+			if(href.indexOf('#') > 0) { //Check if it's an anchor
+				a[i].href = 'http://' + location.hostname + location.pathname + location.search + '#' + a[i].hash.substring(1); //Then rewrite URL with support for empty anchor
+			}
+			else if(href.indexOf('//raw.github.com') > 0 || href.indexOf('//bitbucket.org') > 0) { //Check if it's from raw.github.com or bitbucket.org
+				a[i].href = 'http://' + location.hostname + location.pathname + '?' + href; //Then rewrite URL so it can be loaded using YQL
 			}
 		}
-		if(document.getElementsByTagName('frameset').length)
+		if(document.querySelectorAll('frameset').length)
 			return; //Don't replace CSS/JS if it's a frameset, because it will be erased by document.write()
-		link = document.getElementsByTagName('link');
+		link = document.getElementsByTagName('link[rel=stylesheet][href]');
 		for(i = 0; i < link.length; ++i) {
-			if(link[i].rel
-			&& link[i].rel.toLowerCase() == 'stylesheet'
-			&& link[i].href) {
-				href = link[i].href; //Get absolute URL
-				if(href.indexOf('//raw.github.com') > 0 || href.indexOf('//bitbucket.org') > 0) { //Check if it's from raw.github.com or bitbucket.org
-					HTMLPreview.send(href, 'loadCSS'); //Then load it using YQL
-				}
+			href = link[i].href; //Get absolute URL
+			if(href.indexOf('//raw.github.com') > 0 || href.indexOf('//bitbucket.org') > 0) { //Check if it's from raw.github.com or bitbucket.org
+				HTMLPreview.send(href, 'loadCSS'); //Then load it using YQL
 			}
 		}
-		script = document.getElementsByTagName('script');
+		script = document.querySelectorAll('script[src]');
 		for(i = 0; i < script.length; ++i) {
-			if(script[i].src) {
-				src = script[i].src; //Get absolute URL
-				if(src.indexOf('//raw.github.com') > 0 || src.indexOf('//bitbucket.org') > 0) { //Check if it's from raw.github.com or bitbucket.org
-					HTMLPreview.send(src, 'loadJS'); //Then load it using YQL
-				}
+			src = script[i].src; //Get absolute URL
+			if(src.indexOf('//raw.github.com') > 0 || src.indexOf('//bitbucket.org') > 0) { //Check if it's from raw.github.com or bitbucket.org
+				HTMLPreview.send(src, 'loadJS'); //Then load it using YQL
 			}
 			else if(script[i].innerHTML.indexOf('HTMLPreview') < 0) { //Move all inline scripts except HTMLPreview.replaceAssets()
 				document.write(script[i].outerHTML);
--- readme.md
+++ readme.md
@@ -1,13 +1,16 @@
-Many GitHub repositories don't use GitHub Pages to host their HTML files. GitHub HTML Preview allows you to render those files without cloning or downloading whole repositories.
+Many GitHub repositories don't use GitHub Pages to host their HTML files. **GitHub & BitBucket HTML Preview** allows you to render those files without cloning or downloading whole repositories. It is a  client-side solution and does not involve any third party hosting servers (except for Yahoo! Query Language to fetch assets).
 
-If you try to open raw versions of any HTML, CSS or JS files in a web browser directly from GitHub, all you will see are sources. GitHub forces them to use the "text/plain" content-type. This script overrides it by using Yahoo! Query Language.
+If you try to open raw versions of any HTML, CSS or JS files in a web browser directly from GitHub, all you will see are sources. GitHub forces them to use the "text/plain" content-type, so they cannot be interpreted. This script overrides it by using Yahoo! Query Language.
 
-**Now it works with BitBucket too!**
-
-To use it, just prepend this to the URL of any HTML file: **http://htmlpreview.github.io/?**
+In order to use it, just prepend this fragment to the URL of any HTML file: **[http://htmlpreview.github.io/?](http://htmlpreview.github.io/?)**
 
 E.g.:
 http://htmlpreview.github.io/?https://raw.github.com/twbs/bootstrap/gh-pages/2.3.2/index.html
 http://htmlpreview.github.io/?https://github.com/documentcloud/backbone/blob/master/examples/todos/index.html
 
-GitHub HTML Preview is tested under Google Chrome, Apple Safari and Mozilla Firefox, and it should work with any websites, not only GitHub.
+What it does is load HTML using YQL, then process all links, frames, scripts and styles, and load each of them using YQL, so they can be evaluted in the browser. Here is the workflow:
+```
+HTMLPreview.init() -> HTMLPreview.send(HTML) -> YQL fetch HTML -> HTMLPreview.loadHTML(data) -> HTMLPreview.replaceAssets() -> HTMLPreview.send(CSS) -> YQL fetch CSS -> HTMLPreview.loadCSS(data) -> HTMLPreview.send(JS) -> YQL fetch JS -> HTMLPreview.loadJS(data)
+```
+
+**GitHub & BitBucket HTML Preview** was tested under Google Chrome, Apple Safari and Mozilla Firefox, and it should work with majority of websites, not only GitHub & BitBucket.
