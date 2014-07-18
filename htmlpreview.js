a2f6ff86ba40a6d6d9147e19b4396a4c81310f98
niu tech <jerzyglowacki@gmail.com>
2014-07-18 13:56:46 +0200

    Rewrite only links to HTML files
    Fixes #25

--- htmlpreview.js
+++ htmlpreview.js
@@ -27,7 +27,7 @@ var HTMLPreview = {
 			if(href.indexOf('#') > 0) { //Check if it's an anchor
 				a[i].href = 'http://' + location.hostname + location.pathname + location.search + '#' + a[i].hash.substring(1); //Then rewrite URL with support for empty anchor
 			}
-			else if(href.indexOf('//raw.githubusercontent.com') > 0 || href.indexOf('//bitbucket.org') > 0) { //Check if it's from raw.github.com or bitbucket.org
+			else if((href.indexOf('//raw.githubusercontent.com') > 0 || href.indexOf('//bitbucket.org') > 0) && (href.indexOf('.html') > 0 || href.indexOf('.htm') > 0)) { //Check if it's from raw.github.com or bitbucket.org and to HTML files
 				a[i].href = 'http://' + location.hostname + location.pathname + '?' + href; //Then rewrite URL so it can be loaded using YQL
 			}
 		}
